a3a08b0bd0b4962ebfd8a262d8928a5b700b27a7
Master J <jake.yanov@gmail.com>
2020-11-18 13:25:00 +0200

    sidebar

--- client/src/components/UI/UI.js
+++ client/src/components/UI/UI.js
@@ -4,6 +4,8 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 
+import ResponsiveDrawer from "./SideDrawer";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -20,15 +22,11 @@ export default function UI(props) {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={12} md={12} lg={2}>
-          <Paper className={classes.paper}>Navigation</Paper>
-        </Grid>
-        <Grid item xs={12} sm={12} md={9} lg={8}>
+      <ResponsiveDrawer />
+      <Grid container>
+        <Grid item sm={4} md={3} lg={2}></Grid>
+        <Grid item sm={8} md={9} lg={10}>
           <Paper className={classes.paper}>{props.children}</Paper>
-        </Grid>
-        <Grid item xs={12} sm={12} md={3} lg={2}>
-          <Paper className={classes.paper}>Categories</Paper>
         </Grid>
       </Grid>
     </div>
